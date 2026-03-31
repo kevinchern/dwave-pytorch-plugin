3adb35954b17cc163f5e4d2af1c7ed6116421dd6
Jack Raymond <10591246+jackraymond@users.noreply.github.com>
2026-03-31 12:44:57 -0700

    reformat

--- examples/mmd_ae.py
+++ examples/mmd_ae.py
@@ -597,8 +597,7 @@ def save_gen_multiple_methods(
             )
             num_reads_per_srt = num_reads // num_programming_transformations
         else:
-            num_reads_per_srt = 1
-        sample_params["num_reads"] = num_reads_per_srt
+            num_reads_per_srt = num_reads
 
         for use_automorphisms in [False, True]:
             if use_automorphisms:
@@ -606,6 +605,8 @@ def save_gen_multiple_methods(
                 sample_params["num_reads"] = (
                     num_reads_per_srt // num_programming_transformations
                 )  # Safe by loop ordering
+            else:
+                sample_params["num_reads"] = num_reads_per_srt
             print("DEBUG statement", sample_params)
             sampler = get_sampler(
                 qpu, emb, use_srts, use_automorphisms, grbm.edges, seed
@@ -624,6 +625,7 @@ def save_gen_multiple_methods(
             ), f"Expected num_reads to be 400 after adjusting for SRTs and automorphisms q.shape={q.shape} sample_params0={sample_params0}"
             save_gen(
                 model,
+                f"{title}_S{use_srts}A{use_automorphisms}NPT{num_programming_transformations}",
                 q,
             )
 
